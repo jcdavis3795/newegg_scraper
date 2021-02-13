f45efe3f4f9c463ddf4849590231dc71d8f8927c
Jordan <46761218+jcdavis3795@users.noreply.github.com>
2021-02-13 16:12:22 -0600

    Add files via upload

---
+++ newegg_scraper/funcs/print_results.py
@@ -0,0 +1,8 @@
+def print_results(result_list, num_results):
+
+    index = 0
+
+    for result in range(num_results):
+        print(result_list[index])
+
+        index += 1
---
+++ newegg_scraper/ne_scrape.py
@@ -0,0 +1,19 @@
+from funcs import get_ne_components
+import sys
+# command line functionality. From the terminal in the project directory, run 'ne_scrape.py' the way you would normally
+# run a python file followed by two additional arguments: component type and the number of items you want returned
+
+if __name__ == '__main__':
+    try:
+        if sys.argv[1] in ('cpu', 'gpu', 'mobo_intel', 'mobo_amd', 'memory', 'psu', 'hdd', 'ssd', 'cpu_fan', 'case_fan', 'case'):
+            get_ne_components.get_ne_components(sys.argv[1], int(sys.argv[2]))
+
+        elif sys.argv[1] not in ('cpu', 'gpu', 'mobo_intel', 'mobo_amd', 'memory', 'psu', 'hdd', 'ssd', 'cpu_fan', 'case_fan', 'case'):
+            print('error: component type not found')
+
+        elif sys.argv[2] is not int:
+            print('Specify the number of results you want returned with an integer.')
+
+    except IndexError:
+        print('Please enter your query in the correct format: '
+              '"python3 get_ne_components.py [component type] [num of items to return]"')
